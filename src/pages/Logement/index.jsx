13273df40f207c26996cad3b22b3b6826bf2a903
aurelieweb <web.ademetrio@gmail.com>
2023-08-05 09:31:45 +0200

    modification page logement

--- src/pages/Logement/index.jsx
+++ src/pages/Logement/index.jsx
@@ -6,34 +6,46 @@ import logementData from '../../data/logements.json';
 import Collapse from '../../components/Collapse';
 
 function Logement() {
+  // Utilisation de la fonction useParams() pour récupérer le paramètre "id" de l'URL
   const { id } = useParams();
+
+  // Recherche du logement dans le fichier logements.json en utilisant l'id récupéré de l'URL
   const logement = logementData.find((item) => item.id === id);
 
+  // Vérification si le logement existe, sinon redirige vers la page d'erreur
   if (!logement) {
     return <Navigate to="/error" replace={true} />
   }
 
+  // Affichage du composant Logement
   return (
     <div className="main logement-page">
+      {/* Affichage du composant Slideshow avec les images du logement */}
       <Slideshow logement={logement} />
+
+      {/* Affichage du composant HeaderLogement avec les détails du logement */}
       <HeaderLogement logement={logement} />
-        <Collapse className='collapse-row'
-          data={[
-            { title: 'Description', content: logement.description },
-            {
-              title: 'Équipements',
-              content: (
-                <ul>
-                  {logement.equipments.map((equipment, index) => (
-                    <li key={index}>{equipment}</li>
-                  ))}
-                </ul>
-              ),
-            },
-          ]}
-        />
+
+      {/* Affichage du composant Collapse avec les informations du logement */}
+      <Collapse className='collapse-row'
+        data={[
+          { title: 'Description', content: logement.description },
+          {
+            title: 'Équipements',
+            content: (
+              // Affichage de la liste des équipements du logement
+              <ul>
+                {logement.equipments.map((equipment, index) => (
+                  <li key={index}>{equipment}</li>
+                ))}
+              </ul>
+            ),
+          },
+        ]}
+      />
     </div>
   );
 }
 
 export default Logement;
+
